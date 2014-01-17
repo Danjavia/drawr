1980db33cf5798d892752d0ad6607e77a71dc49d
Danny Viasus <danjavia.iconshock@gmail.com>
2014-01-17 01:48:21 -0500

    new

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('drawrApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
